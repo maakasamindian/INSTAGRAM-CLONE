561256e500036cd2260f469dbf2bdecf1a833e7f
uagalaxy <74304156+uagalaxy@users.noreply.github.com>
2024-05-18 17:47:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-       apiKey: "AIzaSyDix2b-xCtZ1zabAy3muIRpv307zJ6sXoA",
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDix2b-xCtZ1zabAy3muIRpv307zJ6sXoA",
   authDomain: "send-esms.firebaseapp.com",
   databaseURL: "https://send-esms-default-rtdb.firebaseio.com",
   projectId: "send-esms",
   storageBucket: "send-esms.appspot.com",
   messagingSenderId: "304041024188",
   appId: "1:304041024188:web:370ae3e067e6c2f5d60eb9"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+};
+firebase.initializeApp(config);
 
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
 
+// Listen for form submit
+document.addEventListener('submit', submitForm);
 
-	function writeData() {
-        firebase.database().ref("User").set({
-            name: document.getElementById("email").value,
-            Age: document.getElementById ("name").value
-        });
-	}
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  //Get value
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+
+  // Save message
+  saveMessage(name, email);
+ // Show alert
+ document.querySelector('.alert').style.display = 'block';
+
+// Hide alert after 3 seconds
+setTimeout(function(){
+  document.querySelector('.alert').style.display = 'none';
+},3000);
+
+// Clear form
+document.getElementById('contactForm').reset();
+}
+
+// Function to get form value
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name,email){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email
+  });
+}
